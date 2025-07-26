4227eb7863d00d0b6e8cb9b042b7771b5af1f2fe
nyxparadox <paradoxzsite@gmail.com>
2025-07-27 03:03:03 +0530

    Added SignUP screen ui with email and phone.

--- flutter_App/doorsnap/lib/Presentation/home/screen/auth/SignUp_screens/email_phone_screen.dart
+++ flutter_App/doorsnap/lib/Presentation/home/screen/auth/SignUp_screens/email_phone_screen.dart
@@ -11,28 +11,119 @@ class _EmailPhoneScreenState extends State<EmailPhoneScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text('SignUP '),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(30),
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: Text(
-              'Step 1',
-              style: const TextStyle(fontSize: 16, color: Colors.black),
-            ),
-          ),
-        ),
+        title: const Text('SignUP ',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+        
     
       ),
 
       body: Center(
-        child: Text(
-          'Email and Phone Number ',
-          style: TextStyle(fontSize: 24, color: Colors.black),
-        ),
-      ),
-      
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 192, 203, 209),
+          ),
+
+          padding: const EdgeInsets.only( left: 25, right: 25),
+
+          child: Center(
+            child: Container(
+              width: double.infinity,
+              height: 800,
+              decoration: BoxDecoration(
+                color: Colors.white38,
+                borderRadius: BorderRadius.circular(40),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  )
+                ]
+              ),
+            
+              padding: const EdgeInsets.only(top: 50, bottom: 50, left: 25, right: 25),
+            
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("Step 1.",
+                          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    
+                    Text("Please enter your email and phone number to continue.",
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black54),
+                    ),
+                
+                    const SizedBox(height: 40),
+                
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        prefixIcon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)
+                        )
+                      ),                      
+                    ),
+                
+                    const SizedBox(height: 40),
+                
+                    TextField(
+                      keyboardType: TextInputType.number,                      
+                      decoration: InputDecoration(
+                        labelText: "Phone",
+                        prefixIcon: Icon(Icons.phone),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)
+                        )
+                      ),
+                      
+                    ),
+                
+                    const SizedBox(height: 80),
+                
+                    ElevatedButton(onPressed: (){},
+                     child: Text("Next",
+                      style: TextStyle(fontSize: 22,color: Colors.white)),
+                      
+                     
+                     style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 38, 79, 151),
+                      minimumSize: Size(260, 65)
+                      ),
+                    ),
+                
+                    const SizedBox(height: 210,),
+                
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(Icons.info_outline),
+                        Flexible(
+                          child: Text(
+                            "please gave your active email and phone number so our services can reach to you ",
+                            style: TextStyle(color: Colors.grey, fontSize: 14),                            
+                          ),
+                        ),
+                      ],
+                    )               
+                  ],
+                ),
+              ),              
+            ),
+          ),
+        )
+      ),     
     );
   }
 }
